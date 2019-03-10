3746af4955926bcaa78915bb604826d5c04d1c29
易昊 <hao.yick@qq.com>
2019-03-10 11:50:31 +0800

    Create search-a-2d-matrix-ii.py

---
+++ src/search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for i in matrix:
+            for k in i:
+                if k==target:
+                    return True
+        return False    
