2137cb0e9c996902e9f5212b0d6cec6808f1ec0c
易昊 <hao.yick@qq.com>
2019-03-10 11:24:21 +0800

    Delete majority-element.py

--- scr/majority-element/majority-element.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        count={}
-        for i in nums:
-            count.setdefault(i,0)
-            count[i]=count[i]+1
-            if count[i]>len(nums)/2:
-                return i
